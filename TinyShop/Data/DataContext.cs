9236d8d89d326ac49bc45db1406434d0e62a8919
toowoot <1539794351@qq.com>
2020-06-13 23:13:09 +0800

    commit message

---
+++ TinyShop/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class DataContext
+    {
+         public DataContext(DbContextOptions<DataContext> options): base(options) { 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ProductDO>().ToTable("Products");
+        }
+    }
+}
